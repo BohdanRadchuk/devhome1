e81986c126f03459cb21c70bf1b7e2a1c2366c44
BohdanRadchuk <shpionuse@gmail.com>
2018-01-10 13:31:58 +0200

    1

--- 3.sql
+++ 3.sql
@@ -2,4 +2,4 @@ select  sum(salary)
 from developers, developer_skill 
 	where  developers.id_dev IN (
 	select DISTINCT developer_skill.id_dev
-		where (developer_skill.id_skill = 1 or developer_skill.id_skill = 2 or developer_skill.id_skill = 3 ));
+		where (developer_skill.id_skill between 1 and 3 ));
